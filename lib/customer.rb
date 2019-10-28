f041ee90ae1b22e24a91aaa55a6b06d8d8720f17
alycia hall <amhall25@gmail.com>
2019-10-28 00:19:39 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,31 @@
 class Customer
+  attr_accessor :name, :age
+
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+    @meals=[]
+  end
+
+  def self.all
+    @@all
+  end
   
+  def new_meal(waiter, total, tip)
+    meal = Meal.new(waiter, self, total, tip)
+    @meals<< meal
+  end
+  
+  def meals
+    @meals
+  end
+  
+  def waiters
+    @meals.map do |meal|
+      meal.waiter
+    end
+  end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,28 @@
 class Waiter
+attr_accessor :name, :years_experience
 
+  @@all = []
+
+  def initialize(name, years_experience)
+    @name = name
+    @years_experience = years_experience
+    @@all << self
+    @meals=[]
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip)
+    meal = Meal.new(self, customer, total, tip)
+    @meals << meal
+      
+  end
+  
+  def meals
+    @meals
+    
+  end
+  
 end
